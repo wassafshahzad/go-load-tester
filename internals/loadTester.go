3424de933c7c7321284b06de7551bed70b40f9bf
wassafshahzad <wassafshahzad@gmail.com>
2024-08-13 00:34:09 +0200

    few changes to tester

--- internals/loadTester.go
+++ internals/loadTester.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 type HttpMethods string
@@ -17,9 +18,12 @@ const (
 	PATCH HttpMethods = "PATCH"
 )
 
+var clientWithTimeOut http.Client
+
 type Urls struct {
-	Urls     []entities `json:"urls"`
-	Requests int        `json:"requests"`
+	Urls     []entities    `json:"urls"`
+	Requests int           `json:"requests"`
+	Timeout  time.Duration `json:"timeout"`
 }
 
 type entities struct {
@@ -28,25 +32,41 @@ type entities struct {
 	results    requestResult
 }
 
+func (entity entities) GetRequestsResult() (int, int) {
+	return entity.results.dropCount, entity.results.failCount
+}
+
 type requestResult struct {
 	dropCount int
 	failCount int
 }
 
+func init() {
+	clientWithTimeOut = http.Client{
+		Timeout: 5 * time.Second,
+	}
+}
+
 func CallUrl(entity *entities) {
-	req, _ := http.NewRequest(string(entity.HttpMethod), entity.Path, nil)
+	req, err := http.NewRequest(string(entity.HttpMethod), entity.Path, nil)
 	req.Header.Set("Content-Type", "application/json")
 
-	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := clientWithTimeOut.Do(req)
+
 	if err != nil {
 		entity.results.dropCount += 1
 		return
 	}
-
 	defer res.Body.Close()
 
 	if res.StatusCode >= 400 {
 		entity.results.failCount += 1
+	} else {
+		fmt.Printf("%v\n", res.StatusCode)
 	}
 }
 
@@ -60,10 +80,15 @@ func ReadConfig(name string) *Urls {
 
 	defer jsonFile.Close()
 	byteValue, _ := io.ReadAll(jsonFile)
-	json.Unmarshal(byteValue, &apis)
+	_ = json.Unmarshal(byteValue, &apis)
 
 	if apis.Requests == 0 {
 		apis.Requests = 100
 	}
+
+	if apis.Timeout != 0 {
+		clientWithTimeOut.Timeout = apis.Timeout * time.Second
+	}
+
 	return &apis
 }
--- main.go
+++ main.go
@@ -12,18 +12,21 @@ func main() {
 	api := internals.ReadConfig("requests.json")
 
 	var waitGroup sync.WaitGroup
-
-	for _, val := range api.Urls {
-		waitGroup.Add(api.Requests)
+	c := 1
+	for index := range api.Urls {
 		for i := 0; i < api.Requests; i++ {
+			waitGroup.Add(1)
 			go func() {
-				defer waitGroup.Done()
-				fmt.Printf("Sending request at %v \n", val.Path)
-				internals.CallUrl(&val)
+				defer func() {
+					waitGroup.Done()
+					fmt.Printf("Done please %v \n", i)
+				}()
+				internals.CallUrl(&api.Urls[index])
+				fmt.Printf("Calling thread %v \n", i)
 			}()
-
 		}
 	}
 	waitGroup.Wait()
-	fmt.Printf("%v", api)
+	d, f := api.Urls[0].GetRequestsResult()
+	fmt.Printf("Total Requests Dropped %v Failed %v", d, f)
 }
